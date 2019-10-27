ef4eac9358296811006a2082ed3b3015d3a02265
Martin Kunc <5441732+martinkunc@users.noreply.github.com>
2019-10-27 14:42:59 +0100

    remove preventdefault

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 var $form = $('form#test-form'),
     url = 'https://script.google.com/macros/s/AKfycby6e4ELsEzaOUYuO4E5PAYib5pKDl6H12uiZjNQlwvcX_2svMuv/exec'
 
-function submitForm(e) {
-  e.preventDefault();
+function submitForm() {
+  //e.preventDefault();
   var jqxhr = $.ajax({
     url: url,
     method: "GET",
     dataType: "json",
     data: $form.serializeObject()
   }).success(
+    console.log("Success")
     // do something
   );
 }
