544a189f19211460dd693472109d4e3580d2f41c
Martin Kunc <5441732+martinkunc@users.noreply.github.com>
2019-10-27 15:18:28 +0100

    save

--- save.php
+++ save.php
@@ -4,6 +4,10 @@ $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 $input = json_decode(file_get_contents('php://input'),true);
 
+$fp = fopen('data.csv', 'w');
+fwrite($fp, $input);
+fclose($fp);
+
 echo ("{\"status\":\"OK\"}");
 
 ?>
