e20256e90974995d075ade0777825306c56b6bfa
Martin Kunc <5441732+martinkunc@users.noreply.github.com>
2019-10-27 15:14:07 +0100

    save in php

---
+++ save.php
@@ -0,0 +1,6 @@
+// get the HTTP method, path and body of the request
+$method = $_SERVER['REQUEST_METHOD'];
+$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+$input = json_decode(file_get_contents('php://input'),true);
+
+echo ("{\"status\":\"OK\"}");
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var $form = $('form#test-form'),
-    url = 'https://script.google.com/macros/s/AKfycby6e4ELsEzaOUYuO4E5PAYib5pKDl6H12uiZjNQlwvcX_2svMuv/exec'
+    //url = 'https://script.google.com/macros/s/AKfycby6e4ELsEzaOUYuO4E5PAYib5pKDl6H12uiZjNQlwvcX_2svMuv/exec'
+    url = 'https://mkform.herokuapp.com/save.php'
 
 function submitForm() {
   //e.preventDefault();
